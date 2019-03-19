5fceff2f5b8b05a13ad60f974484cf74232849eb
jchinnici <joseph.t.chinnici@gmail.com>
2019-03-18 20:57:59 -0400

    added code to navigate to c# page

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp4
@@ -10,6 +16,12 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.stackoverflow.com");
+
+            IWebElement searchBox = driver.FindElement(By.ClassName("s-input"));
+            searchBox.SendKeys("c#");
+            driver.FindElement(By.ClassName("s-btn")).Click();
         }
     }
 }
